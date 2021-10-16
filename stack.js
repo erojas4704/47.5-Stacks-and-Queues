4b9f8c052dd3779817bc32e38b87c28bcda7fd6e
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-10-16 14:41:09 -0400

    Complete

---
+++ stack.js
@@ -0,0 +1,64 @@
+/** Node: node for a stack. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** Stack: chained-together nodes where you can
+ *  remove from the top or add to the top. */
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /** push(val): add new value to end of the stack. Returns undefined. */
+
+  push(val) {
+    const node = new Node(val);
+    if (this.first === null) {
+      this.first = this.last = node;
+      this.size = 1;
+      return;
+    }
+    node.next = this.first;
+    this.first = node;
+    this.size++;
+  }
+
+  /** pop(): remove the node from the top of the stack
+   * and return its value. Should throw an error if the stack is empty. */
+
+  pop() {
+    if(!this.first) throw new ReferenceError("Attempted to access an empty stack.");
+    const node = this.first;
+    if (this.first === node.last) {
+      this.size = 0;
+      this.first = this.last = null;
+      return node.val;
+    }
+
+    this.size --;
+    this.first = node.next;
+    return node.val;
+  }
+
+  /** peek(): return the value of the first node in the stack. */
+
+  peek() {
+    return this.first.val;
+  }
+
+  /** isEmpty(): return true if the stack is empty, otherwise false */
+
+  isEmpty() {
+    return this.first === null;
+  }
+}
+
+module.exports = Stack;
